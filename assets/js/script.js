48a832e22e5c2206cadd80f5cb5f9654a58d0509
enlace-de <prisypau81@gmail.com>
2024-01-15 12:55:40 -0400

    se agrega btn agregar como aplicacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,26 +64,33 @@ function copiarAlPortapapeles() {
 
 favoritoIcon.addEventListener('click', copiarAlPortapapeles);
 
-// animacion logo
-// Obtiene la referencia a la imagen por su ID
+// funcion agregar a pagina de inicio
 
-const logoImage = document.getElementById('logoImage');
+let deferredPrompt;
 
-// Función para iniciar la animación
-function iniciarAnimacion() {
-    logoImage.classList.add('animacion');
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+function addToHomeScreen() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  } else {
+    alert("Para agregar a la pantalla de inicio, ve al menú de la barra inferior y selecciona COMPARTIR (cuadrado con una flecha hacia arriba), después AGREGAR A LA PANTALLA DE INICIO, finalmente AGREGAR ubicado en la esquina superior derecha.");
+  }
 }
 
-// Agrega un controlador de eventos al cargar la página
-window.addEventListener('load', iniciarAnimacion);
-
-// Agrega un controlador de eventos al hacer clic en la imagen
-logoImage.addEventListener('click', () => {
-    // Reinicia la animación al hacer clic
-    logoImage.classList.remove('animacion');
-    void logoImage.offsetWidth; // Esto fuerza un reinicio de la animación
-    logoImage.classList.add('animacion');
-});
 
 
 
